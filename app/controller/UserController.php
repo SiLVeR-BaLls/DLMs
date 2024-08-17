a9eaafee450fc407bc125b0df08de8e501cf6405
glendecado <131495980+glendecado@users.noreply.github.com>
2024-08-17 14:09:28 +0000

    model and controller connection to sql

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -1,14 +1,16 @@
 <?php
-
+include 'app/model/user.php';
 class UserController {
 
-public $data;
+    public $data;
+    public $model;
 
     public function __construct($data){
         $this->data = $data;
+        $this->model = new user();
     }
 
-    public function test(){
-        return $this->data;
+    public function sign_up(){
+        $this->model->sign_up();
     }
 }
--- app/includes/inc_sign_up.php
+++ app/includes/inc_sign_up.php
@@ -6,7 +6,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = new UserController($_POST);
 }
 
-
-
 ?>
-<pre> <?php echo var_dump($user->test()) ?> </pre>
